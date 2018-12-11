2f9b084b1ea9765503040467a26ebb719162647c
Vlad Abramov <neoandmatrix2@gmail.com>
2018-12-11 05:13:10 +0300

    ready version

--- js/common.js
+++ js/common.js
@@ -1,5 +1,32 @@
-$(function() {
 
-	// Custom JS
-
+$(document).ready(function () {
+	$('.mobile-menu').hide();
+	$('.mobile-line').hide();
+	$('.burger').click(function () {
+		$(this).find('.burger__icon').toggleClass('burger__icon--active');
+		$('header').find('.mobile-menu').slideToggle();
+		$('.mobile-line').slideToggle();
+	});
+	bgSectionHeight()
+	$(window).on('resize', function () {
+		bgSectionHeight()
+	});
+	var phoneMask = new IMask(document.getElementById('phone'), {
+		mask: '+{7} (000) 000 00 00'
+	});
+	$('#preloader').find('i').fadeOut().end().delay(200).fadeOut('slow');
 });
+
+function goTo(name) {
+	$('html, body').animate({
+		scrollTop: $("#" + name).offset().top
+	}, 600);
+}
+
+function bgSectionHeight() {
+	if ($(window).height() * 0.8 > 800) {
+		$('.main').css("min-height", "800px");
+	} else {
+		$('.main').css("min-height", $(window).height() * 0.8 + 64 + "px")
+	}
+}
